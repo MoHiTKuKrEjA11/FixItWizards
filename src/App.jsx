0a93039cc802d6a562addb9a4b20f2d5933c45fd
Mohit Kumar <mohitkd4444@gmail.com>
2023-09-16 21:43:33 +0530

    modified:   src/App.jsx 	deleted:    src/components/Navbar.css 	modified:   src/components/navbar.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Navbar from "./components/navbar";
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      
       <Navbar/>
-    </h1>
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,16 @@
-// src/components/Navbar.js
 import React from 'react';
-import './Navbar.css';
+// import './Navbar.css';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="container">
-        <a className="navbar-brand" href="/">FixItWizards</a>
+        <a className="font-sans navbar-brand" href="/">FixItWizards</a>
         <ul className="navbar-nav">
-          <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-          <li className="nav-item"><a className="nav-link" href="/contact">Contact</a></li>
-          <li className="nav-item"><a className="nav-link" href="/service">Chat</a></li>
-          <li className="nav-item"><a className="nav-link" href="/alumni">Connect with alumni</a></li>
+          <li className="font-sans nav-item"><a className="nav-link" href="/">Home</a></li>
+          <li className="font-sans nav-item"><a className="nav-link" href="/contact">Contact</a></li>
+          <li className="font-sans nav-item"><a className="nav-link" href="/service">Chat</a></li>
+          <li className="font-mono nav-item"><a className="nav-link" href="/alumni">Connect with alumni</a></li>
         </ul>
       </div>
     </nav>
