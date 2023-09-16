e3c289262125da4b91835dc54bd2af6b96b0e4b6
Mohit Kumar <mohitkd4444@gmail.com>
2023-09-16 22:43:03 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/assets/icons/hamburger.svg 	modified:   src/components/navbar.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar";
-
 export default function App() {
   return (
+    <Router>
       <Navbar/>
+    </Router>
+      
   )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-// import './Navbar.css';
-
+import  hamburger  from "../assets/icons/hamburger.svg";
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="container">
-        <a className="font-sans navbar-brand" href="/">FixItWizards</a>
-        <ul className="navbar-nav">
-          <li className="font-sans nav-item"><a className="nav-link" href="/">Home</a></li>
-          <li className="font-sans nav-item"><a className="nav-link" href="/contact">Contact</a></li>
-          <li className="font-sans nav-item"><a className="nav-link" href="/service">Chat</a></li>
-          <li className="font-mono nav-item"><a className="nav-link" href="/alumni">Connect with alumni</a></li>
+    <header className='absolute z-10 w-full py-8 padding-x'>
+      <nav className='flex items-center justify-between max-container'>
+      <h1 className='text-lg'>FixItWizards</h1>
+        <ul className='flex items-center justify-center flex-1 gap-16 max-md:hidden'>
+          <li>Home</li>
+          <li>Contact</li>
+          <li>Services</li>
         </ul>
-      </div>
-    </nav>
+        <div className='flex gap-2 text-lg font-medium leading-normal font-montserrat max-lg:hidden wide:mr-24'>
+          <a href='/'>Sign in</a>
+          <span>/</span>
+          <a href='/'>Explore now</a>
+        </div>
+        <div className='hidden max-md:block'>
+          <img src={hamburger} alt='hamburger icon' width={25} height={25} />
+        </div>
+      </nav>
+    </header>
   );
 }
 
