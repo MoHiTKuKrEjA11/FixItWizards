17f7d3475a32a7771f353745d8ccc6af88e09c44
MoHiTKuKrEjA11 <mohitkukreja463@gmail.com>
2023-09-20 16:00:34 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/components/SignRegister/SignRegister.jsx 	new file:   src/components/SignRegister/SignRegistercs.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/navbar";
+import SignRegister from "./components/SignRegister/SignRegister"
 export default function App() {
   return (
     <Router>
       <Navbar/>
+      <SignRegister/>
     </Router>
       
   )
